213795ff58b12908bc6f34a88000996cd2a97d08
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-06-20 21:01:36 +0200

    Projekt neu erzeugt mit Spring Initializr https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.3.1&packaging=jar&jvmVersion=17&groupId=de.eldecker.dhbw.spring&artifactId=passwortgrab&name=Passwortgrab&description=Passwortmanager&packageName=de.eldecker.dhbw.spring.passwortgrab&dependencies=h2,data-jpa,thymeleaf

---
+++ src/main/java/de/eldecker/dhbw/spring/passwortgrab/PasswortgrabApplication.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.passwortgrab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswortgrabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswortgrabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/eldecker/dhbw/spring/passwortgrab/PasswortgrabApplicationTests.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.passwortgrab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswortgrabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
