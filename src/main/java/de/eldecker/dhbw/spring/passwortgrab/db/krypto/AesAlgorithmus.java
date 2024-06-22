b98b90d4dd68287d87906f42169cbcf062aa15be
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-06-22 15:15:00 +0200

    Detailseite angefangen

--- src/main/java/de/eldecker/dhbw/spring/passwortgrab/db/krypto/AesAlgorithmus.java
+++ src/main/java/de/eldecker/dhbw/spring/passwortgrab/db/krypto/AesAlgorithmus.java
@@ -3,19 +3,34 @@ package de.eldecker.dhbw.spring.passwortgrab.db.krypto;
 import java.util.Base64;
 import java.util.Base64.Decoder;
 import java.util.Base64.Encoder;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.security.GeneralSecurityException;
 import java.security.SecureRandom;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import jakarta.annotation.PostConstruct;
+
 
 /**
  * Diese Klasse enthält die Methoden für die Verschlüsselung und Entschlüsselung
- * mit dem symmetrischen Verschlüsselungsalgorithmus "AES".
+ * mit dem symmetrischen Verschlüsselungsalgorithmus "AES" im Betriebsmodus
+ * "CBC" (Chiper Block Mode).
  */
 @Service
 public class AesAlgorithmus {
+    
+    /**
+     * Genauer Bezeichner Verschlüsselungsalgorithmus:
+     * <ul>
+     * <li>AES: Symmetrischer Verschlüsselungsalgorithmus, Block-Chiffre</li>
+     * <li>CBC: Betriebsart "Chiper Block Mode", braucht aber Initialisierungsvektor (IV).</li>
+     * <li>PKCS5Padding: Algo für Füll-Bytes, um Block bei Bedarf aufzufüllen.</li>
+     * </ul>
+     */
+    private static final String KRYPTO_ALGO_NAME = "AES/ECB/PKCS5Padding";    
     
     /**
      * Symmetrischer Schlüssel (128 Bit) als Hexadezimalziffer mit 32 Buchstaben,
@@ -48,6 +63,33 @@ public class AesAlgorithmus {
 
     /** Sicherer Zufallsgenerator. */
     private SecureRandom _secureRandom = new SecureRandom();
+    
+    
+    /**
+     * Diese Methode wird unmittelbar nach Erzeugung der Bean aufgerufen,
+     * aber erst, wenn der Konstruktor abgearbeitet wurde, so dass der
+     * aus der Datei {@code application.properties} eingelesene symmetrische
+     * Schlüssel zur Verfügung steht.
+     * <br><br>
+     *
+     * Wenn diese Methode eine Exception wirft, dann bricht das Programm ab.
+     * Das ist beabsichtigt, weil die Anwendung nicht sinnvoll funktionieren
+     * kann wenn der Zugriff auf die verschlüsselten Datenbankspalten nicht
+     * möglich ist.
+     *
+     * @throws GeneralSecurityException Fehler bei Ver- oder Entschlüsselung
+     */
+    @PostConstruct
+    private void initialisierung() throws GeneralSecurityException {
+        
+        Pattern hexRegexpPattern = Pattern.compile( "^([0-9a-fA-F]{32})$" );
+        Matcher matcher          = hexRegexpPattern.matcher( _schluesselHex );
+        if ( !matcher.matches() ) {
+            
+            throw new GeneralSecurityException( "Der AES-Schlüssel besteht nicht aus 32 Hex-Ziffern." );
+        }        
+        
+    }
 
 
     /**
@@ -68,6 +110,40 @@ public class AesAlgorithmus {
         _secureRandom.nextBytes( zufallsByteArray );
 
         return _base64Encoder.encodeToString( zufallsByteArray );
+    }
+
+    
+    /**
+     * Klartext verschlüsseln.
+     * 
+     * @param stringKlartext String, der verschlüsselt werden soll
+     * 
+     * @param iv Initialisierungsvektor für Betriebsmodus "CBC"
+     * 
+     * @return Chiffre als Base64-String, wird in Datenbank gespeichert
+     * 
+     * @throws GeneralSecurityException Fehler beim Verschlüsseln
+     */
+    public String verschluesseln( String stringKlartext, String iv ) throws GeneralSecurityException {
+        
+        return "";
+    }
+
+
+    /**
+     * Chiffre entschlüsseln.
+     * 
+     * @param stringChiffre Chiffre als Base64-String (von Datenbank), die entschlüsselt werden soll
+     * 
+     * @param iv Initialisierungsvektor für Betriebsmodus "CBC" 
+     * 
+     * @return Klartext
+     * 
+     * @throws GeneralSecurityException Fehler beim Entschlüsseln
+     */
+    public String entschluesseln( String stringChiffre, String iv ) throws GeneralSecurityException {
+        
+        return "";
     }
 
 }
--- src/main/java/de/eldecker/dhbw/spring/passwortgrab/web/ThymeleafController.java
+++ src/main/java/de/eldecker/dhbw/spring/passwortgrab/web/ThymeleafController.java
@@ -1,16 +1,20 @@
 package de.eldecker.dhbw.spring.passwortgrab.web;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import de.eldecker.dhbw.spring.passwortgrab.db.PasswortEntity;
 import de.eldecker.dhbw.spring.passwortgrab.db.PasswortRepo;
-
 
 
 /**
@@ -19,6 +23,8 @@ import de.eldecker.dhbw.spring.passwortgrab.db.PasswortRepo;
 @Controller
 @RequestMapping( "/app" )
 public class ThymeleafController {
+    
+    private final static Logger LOG = LoggerFactory.getLogger( ThymeleafController.class );
 
     /** Repo-Bean für Zugriff auf Datenbanktabelle mit Passwörtern.. */
     @Autowired
@@ -33,13 +39,46 @@ public class ThymeleafController {
      * @return Name von Template-Datei {@code passwort-liste.html} ohne Datei-Endung 
      */
     @GetMapping( "/passwort-liste" )
-    public String abfrageKfzKennzeichen( Model model ) {
+    public String getPasswortListe( Model model ) {
         
         List<PasswortEntity>  passwortListe = _passwortRepo.findAllSortedByTitle();
         
         model.addAttribute( "passwortListe", passwortListe );
         
         return "passwort-liste";
+    }
+    
+    
+    /**
+     * Details für ein Passwort-Datensatz anzeigen.
+     * 
+     * @param model Objekt für Platzhalterwerte in Template
+     * 
+     * @param id Pfadparameter mit ID des Datensatzes, der angezeigt werden soll 
+     * 
+     * @return Name von Template-Datei {@code passwort-details.html} 
+     *         doer {@code passwort-fehler.html} ohne Datei-Endung
+     */
+    @GetMapping( "/passwort-details/{id}" )
+    public String getPasswortDetails( Model model, 
+                                      @PathVariable Long id ) {
+             
+        Optional<PasswortEntity> passwortOptional = _passwortRepo.findById( id );        
+        if ( passwortOptional.isEmpty() ) {
+            
+            String fehlermeldung = String.format( "Kein Passwort mit ID=%d gefunden.", id );
+            LOG.warn( fehlermeldung );
+            model.addAttribute( "fehlermeldung", fehlermeldung );
+            
+            return "passwort-fehler";
+            
+        } else {
+            
+            PasswortEntity passwortEntity = passwortOptional.get();
+            model.addAttribute( "passwortObjekt", passwortEntity );
+        }
+        
+        return "passwort-details";
     }
     
 }
