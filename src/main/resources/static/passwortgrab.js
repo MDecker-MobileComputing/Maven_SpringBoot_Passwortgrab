962ea32cc4b925d04360f078fc08dce56d47316f
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-06-22 16:42:52 +0200

    passwortgrab.js: Umschaltung Sichtbarkeit implementiert

--- src/main/resources/static/passwortgrab.js
+++ src/main/resources/static/passwortgrab.js
@@ -1,23 +1,51 @@
 "use strict";
 
 
+let passwordInput = null;
+
+/**
+ * Event-Handler-Funktion für Klick auf Button "Sichtbarkeit umschalten".
+ */
+function togglePasswortSichtbarkeit() {
+
+    if (! passwortInput ) {
+
+        console.error( "Input-Element mit ID 'id-passwort' nicht gefunden" );
+        return;
+    }
+
+    if ( passwordInput.type === "password" ) {
+
+        passwordInput.type = "text";
+
+    } else {
+
+        passwordInput.type = "password";
+    }
+}
+
+
+/**
+ * Event-Handler für Initialisierung nachdem DOM von Seite geladen wurde.
+ */
 document.addEventListener( "DOMContentLoaded", function() {    
 
+    passwordInput = document.getElementById( "id-passwort" );
+    if ( !passwordInput ) {
+
+        console.error( "Input-Element mit ID 'id-passwort' nicht gefunden" );
+        return;
+    }
+
     const toggleButton = document.getElementById( "id-toggleButton" );
-    toggleButton.addEventListener( "click", function() {
+    if ( !toggleButton ) {
+            
+        console.error( "Button-Element mit ID 'id-toggleButton' nicht gefunden" );
+        return;
+    }
 
-        const passwordInput = document.getElementById( "id-passwort" );
+    toggleButton.addEventListener( "click", togglePasswortSichtbarkeit );
 
-        if ( passwordInput.type === "password" ) {
-
-            passwordInput.type = "text";
-
-        } else {
-
-            passwordInput.type = "password";
-        }
-    });
-
-    console.log( "ToggleButton initialisiert" );
+    console.log( "JavaScript initialisiert" );
 });
 
